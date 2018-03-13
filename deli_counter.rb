55f4c105e19544c84fa2199680f1847c62e26865
Trevor Seitz <trevinla@gmail.com>
2018-03-13 13:55:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ end
 
 def now_serving(line_array)
   if line_array.size == 0
-    "There is nobody waiting to be served!"
+    puts "There is nobody waiting to be served!"
   else
   current_customer = line_array.shift
   puts "Currently serving #{current_customer}."
