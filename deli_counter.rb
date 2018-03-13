309b25b1a516799d0dcbb2945ae3d1edd386fda8
Trevor Seitz <trevinla@gmail.com>
2018-03-13 13:57:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,8 @@ end
 
 def take_a_number(line_array, customer_name)
   line_array << customer_name
-  puts "The line is currently: #{line_array.index(customer_name) + 1}. #{customer_name}"
+  # puts "The line is currently: #{line_array.index(customer_name) + 1}. #{customer_name}"
+  puts "Welcome, #{customer_name}. You are number #{line_array.index(customer_name) + 1} in line."
 end
 
 def now_serving(line_array)
